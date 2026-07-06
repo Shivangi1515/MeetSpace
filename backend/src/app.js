b303e24dfe4a0ec81d9621108a33ace2bce9a8d3
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-07 03:27:35 +0530

    Initial commit: Set up backend Express API structure and python requirements

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import  {createServer} from "node:http";
+
+import {Server} from "socket.io";
+import  mongoose from "mongoose";
+
+import cors from "cors";
+
+const app=express();
+const server=createServer(app);
+const io=new Server(server)
+
+app.set("port",(process.env.PORT || 8000));
+
+app.get("/home",(req,res)=>{
+    return res.json({"hello":"world"});
+})
+
+const start=async()=>{
+
+    server.listen(app.get("port"),()=>{
+
+        console.log("listening on port 8000!");
+
+    });
+}
+
+start();
