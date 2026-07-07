5b41bfcb6200226dde1b53b148ce3fb4721f3748
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-07 19:20:04 +0530

    feat(backend): configure app server entrypoint, middleware, socket.io, and environment variable integration for MongoDB

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,20 +3,42 @@ import  {createServer} from "node:http";
 
 import {Server} from "socket.io";
 import  mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
 
+import userRoutes from "./routes/users.routes.js";
+
+
 const app=express();
 const server=createServer(app);
-const io=new Server(server)
+const io=connectToSocket(server,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"],
+        allowedHeaders:["*"],
+        credentials: true
+    }
+});
 
 app.set("port",(process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({extended:true,limit:"40kb"}));
 
-app.get("/home",(req,res)=>{
-    return res.json({"hello":"world"});
-})
+app.use("/api/v1/users",userRoutes);
+
+
 
 const start=async()=>{
+    app.set("mongo_user")
+    const connectionDb=await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(`MONGO Connected DB:${connectionDb.connection.host}`)
 
     server.listen(app.get("port"),()=>{
 
